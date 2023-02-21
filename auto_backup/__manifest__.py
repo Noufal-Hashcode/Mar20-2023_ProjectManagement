a81b7550ed384a8e81b72884de8ef34e80f23be6
Noufal - hashcodeit <101789090+Noufal-Hashcode@users.noreply.github.com>
2023-02-21 11:37:25 +0400

    manifest edit

--- auto_backup/__manifest__.py
+++ auto_backup/__manifest__.py
@@ -5,21 +5,20 @@
     'description': """
         The Database Auto-Backup module enables the user to make configurations for the automatic backup of the database. 
         Backups can be taken on the local system or on a remote server, through SFTP.
-        You only have to specify the hostname, port, backup location and databasename (all will be pre-filled by default with correct data.
+        You only have to specify the hostname, port, backup location and data basename (all will be pre-filled by default with correct data.
         If you want to write to an external server with SFTP you will need to provide the IP, username and password for the remote backups.
-        The base of this module is taken from Odoo SA V6.1 (https://www.odoo.com/apps/modules/6.0/auto_backup/) and then upgraded and heavily expanded.
-        This module is made and provided by Yenthe Van Ginneken (Oocademy).
+        The base of this module is taken from Odoo SA V6.1 (https://www.odoo.com/apps/modules/6.0/auto_backup/) 
         Automatic backup for all such configured databases can then be scheduled as follows:  
-                      
         1) Go to Settings / Technical / Automation / Scheduled actions.
         2) Search the action 'Backup scheduler'.
         3) Set it active and choose how often you wish to take backups.
         4) If you want to write backups to a remote location you should fill in the SFTP details.
     """,
-    'author': "Yenthe Van Ginneken",
-    'website': "http://www.odoo.yenthevg.com",
+    'author': "Hash Code IT",
+    'website': "https://www.hashcodeit.com/",
+    'maintainer': "nfl",
     'category': 'Administration',
-    'version': '16.0.0.1',
+    'version': '16.0.1',
     'installable': True,
     'license': 'LGPL-3',
 
